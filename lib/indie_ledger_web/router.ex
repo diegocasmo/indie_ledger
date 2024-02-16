a2e06a007ea57facddea5fcb343023288eb93bb6
Diego Castillo <diegocasmo@gmail.com>
2024-02-16 14:25:14 -0500

    Add authentication code

--- lib/indie_ledger_web/router.ex
+++ lib/indie_ledger_web/router.ex
@@ -1,6 +1,8 @@
 defmodule IndieLedgerWeb.Router do
   use IndieLedgerWeb, :router
 
+  import IndieLedgerWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,16 +10,11 @@ defmodule IndieLedgerWeb.Router do
     plug :put_root_layout, html: {IndieLedgerWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", IndieLedgerWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
   end
 
   # Other scopes may use custom stacks.
@@ -39,6 +36,45 @@ defmodule IndieLedgerWeb.Router do
 
       live_dashboard "/dashboard", metrics: IndieLedgerWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", IndieLedgerWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{IndieLedgerWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", IndieLedgerWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{IndieLedgerWeb.UserAuth, :ensure_authenticated}] do
+      get "/", PageController, :home
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", IndieLedgerWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{IndieLedgerWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
--- test/indie_ledger_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule IndieLedgerWeb.PageControllerTest do
-  use IndieLedgerWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
